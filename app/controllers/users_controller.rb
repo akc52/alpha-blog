c78c4472d1bf7e34c532a7dc89f7b2a4ed2533d8
Alyssa Ciccone <a_lys_sa@hotmail.com>
2019-03-26 17:14:51 -0400

    Complete authentication system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
+  before_action :require_same_user, only: [:edit, :update]
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
   end
@@ -10,7 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Alpha Blog #{@user.username}"
+      flash[:success] = "Welcome to the alpha blog #{@user.username}"
       redirect_to articles_path
     else
       render 'new'
@@ -18,11 +20,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Your account was updated successfully"
       redirect_to articles_path
@@ -32,13 +32,22 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = "You can only edit your own account"
+      redirect_to root_path
+    end
   end
 
 end
